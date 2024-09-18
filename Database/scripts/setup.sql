79dde0314dc84e92ce3f49e6777a95f1a2d8d73d
William Ileka <ileka2468@gmail.com>
2024-09-18 18:43:16 -0500

    changed schema plan added name fields

--- Database/scripts/setup.sql
+++ Database/scripts/setup.sql
@@ -5,6 +5,8 @@
 CREATE TABLE Users (
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
   password VARCHAR(50) NOT NULL, -- HASHED ONLY
   enabled BOOLEAN NOT NULL
 );

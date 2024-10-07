0dd75f9c1818b8fcb37c67e15ce7f65104e7885e
William Ileka <ileka2468@gmail.com>
2024-10-07 12:38:53 -0500

    routing pr review changes

---
+++ Frontend/src/Components/root/Root.jsx
@@ -0,0 +1,52 @@
+import Nav from "../Nav/Nav";
+import LoginModal from "../Authentication/LoginModal";
+import RegisterModal from "../Authentication/RegisterModal";
+import { useDisclosure } from "@nextui-org/react";
+import useUser from "../../Security/hooks/useUser";
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+  const [userData, setUserData, isUser] = useUser();
+  // useDisclosure for Modals
+  const {
+    isOpen: isLoginOpen,
+    onOpen: onLoginOpen,
+    onOpenChange: onLoginOpenChange,
+  } = useDisclosure();
+
+  const {
+    isOpen: isRegisterOpen,
+    onOpen: onRegisterOpen,
+    onOpenChange: onRegisterOpenChange,
+  } = useDisclosure();
+  return (
+    <>
+      <main className="h-full px-4 pt-16">
+        {/* Login Modal */}
+        <LoginModal
+          isOpen={isLoginOpen}
+          onOpen={onLoginOpen}
+          onOpenChange={onLoginOpenChange}
+          setUserData={setUserData}
+          isUser={isUser}
+        />
+        {/* Register Modal */}
+        <RegisterModal
+          isOpen={isRegisterOpen}
+          onOpen={onRegisterOpen}
+          onOpenChange={onRegisterOpenChange}
+          setUserData={setUserData}
+        />
+
+        <Nav
+          onRegisterOpen={onRegisterOpen}
+          onLoginOpen={onLoginOpen}
+          userData={userData}
+          setUserData={setUserData}
+          isUser={isUser}
+        />
+        <Outlet />
+      </main>
+    </>
+  );
+}

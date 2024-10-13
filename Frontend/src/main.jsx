aa40bae4b5a4a0208cca9892e1f93c47c8b2abe4
Matt <MKRUPA4@depaul.edu>
2024-10-13 10:26:44 -0500

    Added routing to message page and messages to nav

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import FindRoommatesPage from "./Pages/FindRoommatesPage.jsx";
+import MessagesPage from "./Pages/MessagesPage.jsx";
 import Root from "./Components/root/Root.jsx";
 import "./index.css";
 import { AxiosProvider } from "./Security/axios/AxiosProvider.jsx";
@@ -10,7 +11,9 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{ path: "find-roommates", element: <FindRoommatesPage /> }],
+    children: [{ path: "find-roommates", element: <FindRoommatesPage /> },
+               { path: "messages", element: <MessagesPage />},
+    ],
   },
 ]);
 

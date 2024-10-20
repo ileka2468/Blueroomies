145314feeed08fb2ca17955b7ec284c8adb30d33
William Ileka <ileka2468@gmail.com>
2024-10-20 03:10:59 -0500

    added ip to log

--- Backend/src/main/java/edu/depaul/cdm/se452/rfa/authentication/security/JwtAuthenticationFilter.java
+++ Backend/src/main/java/edu/depaul/cdm/se452/rfa/authentication/security/JwtAuthenticationFilter.java
@@ -27,6 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        logger.info("IP : " + request.getRemoteAddr());
         String jwt = tokenProvider.getJwtFromRequest(request);
 
         try {
